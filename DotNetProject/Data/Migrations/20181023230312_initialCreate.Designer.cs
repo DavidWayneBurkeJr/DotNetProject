d518faec26afe2f2e4998785190456cd1bdf4acd
ISuckHardAtCoding <dwburkinator@gmail.com>
2018-10-23 20:08:10 -0400

    Blog model

--- DotNetProject/Data/Migrations/20181023230312_initialCreate.Designer.cs
+++ DotNetProject/Data/Migrations/20181023230312_initialCreate.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace DotNetProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181023230312_initialCreate")]
+    partial class initialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DotNetProject/Models/BlogModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetProject.Models
+{
+    public class BlogModel
+    {
+        public string Username { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public BlogModel()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
