56e292ebcd9592dc83e4590c012e20715eeb1c75
ISuckHardAtCoding <dwburkinator@gmail.com>
2018-11-05 19:53:13 -0500

    Init2

--- DotNetProject/Data/Migrations/20181106004359_newMigration.Designer.cs
+++ DotNetProject/Data/Migrations/20181106004359_newMigration.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace DotNetProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181106004359_newMigration")]
+    partial class newMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
